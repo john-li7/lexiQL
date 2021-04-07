838407bbe1bb6c9c6bc6ef79bbca28bf6ea3a0c6
Carthanial <ccarney51@gmail.com>
2021-04-07 14:14:27 -0400

    placeholder

--- server/controllers/GQLController.js
+++ server/controllers/GQLController.js
@@ -1,13 +1,13 @@
 const { createTypes, createResolvers } = require('../GQLFactory/schemaFactory');
-
 const GQLController = {};
-
+const { makeExecutableSchema } = require('/graphql-tools');
 GQLController.createGQLSchema = (req, res, next) => {
   const { SQLSchema } = res.locals;
   try {
     const types = createTypes(SQLSchema);
     const resolvers = createResolvers(SQLSchema);
     res.locals.GQLSchema = { types, resolvers };
+    res.locals.executedSchema = makeExecutableSchema({ types, resolvers });
     return next();
   } catch (err) {
     const errObject = {
@@ -21,4 +21,13 @@ GQLController.createGQLSchema = (req, res, next) => {
   }
 };
 
+/* GQLController.createGQLServer = (req, res, next) => {
+  const PG_URI = req.body.link;
+  const { typeDefs, resolvers } = res.locals.GQLSchema;
+  const pool = new Pool({
+    connectionString: PG_URI,
+  });
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+}; */
 module.exports = GQLController;
--- server/server.js
+++ server/server.js
@@ -1,23 +1,36 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 const path = require('path');
 const router = require('./router');
 const app = express();
-
+const schema = 
+  
+  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // route to dummy db
 app.use('/', router);
 // to only run build and get static when in production, not development
 app.use('/build', express.static(path.join(__dirname, '../build')));
-
+// serves homepage
 app.get('/', (req, res) =>
-  res.status(200).sendFile(path.resolve(__dirname, '../client/index.html')),
+  res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'))
 );
-
+// catch all route handler
 app.use('*', (req, res) => {
   res.status(404).send('Not Found');
 });
-
+// route to graphiql playground
+app.use(
+  '/graphiql',
+  getSQLSchema,
+  createGQLSchema,
+  graphqlHTTP({
+    schema: res.locals.executedSchema,
+    graphiql: true,
+  })
+);
+// global error handler
 app.use((err, req, res, next) => {
   console.log('error handler', err);
   res.status(500).send('Internal Server Error');
